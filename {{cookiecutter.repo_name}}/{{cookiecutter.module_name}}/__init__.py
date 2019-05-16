073ca8061a47c4bf00031ee220a18c0bdf2b5bec
Tobias Kunze <r@rixx.de>
2019-05-17 00:00:23 +0200

    Style setup.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/__init__.py
@@ -3,18 +3,18 @@ from django.utils.translation import ugettext_lazy
 
 
 class PluginApp(AppConfig):
-    name = '{{cookiecutter.module_name}}'
-    verbose_name = '{{cookiecutter.human_name}}'
+    name = "{{cookiecutter.module_name}}"
+    verbose_name = "{{cookiecutter.human_name}}"
 
     class PretalxPluginMeta:
-        name = ugettext_lazy('{{cookiecutter.human_name}}')
-        author = '{{cookiecutter.author_name}}'
+        name = ugettext_lazy("{{cookiecutter.human_name}}")
+        author = "{{cookiecutter.author_name}}"
         description = ugettext_lazy("{{cookiecutter.short_description}}")
         visible = True
-        version = '0.0.0'
+        version = "0.0.0"
 
     def ready(self):
         from . import signals  # NOQA
 
 
-default_app_config = '{{cookiecutter.module_name}}.PluginApp'
+default_app_config = "{{cookiecutter.module_name}}.PluginApp"

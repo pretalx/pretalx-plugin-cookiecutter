efa2875b54e1fb9a9bb43c8cd252960b0b922dc5
Tobias Kunze <r@rixx.de>
2023-07-25 13:01:06 +0200

    Move to pyproject.toml

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,42 +1,4 @@
-import os
-from distutils.command.build import build
-
-from django.core import management
-from setuptools import find_packages, setup
-
-try:
-    with open(
-        os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
-    ) as f:
-        long_description = f.read()
-except FileNotFoundError:
-    long_description = ""
+from setuptools import setup
 
 
-class CustomBuild(build):
-    def run(self):
-        management.call_command("compilemessages", verbosity=1)
-        build.run(self)
-
-
-cmdclass = {"build": CustomBuild}
-
-
-setup(
-    name="{{cookiecutter.repo_name}}",
-    version="0.0.0",
-    description="{{cookiecutter.short_description}}",
-    long_description=long_description,
-    url="{{cookiecutter.repo_url}}",
-    author="{{cookiecutter.author_name}}",
-    author_email="{{cookiecutter.author_email}}",
-    license="Apache Software License",
-    install_requires=[],
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    include_package_data=True,
-    cmdclass=cmdclass,
-    entry_points="""
-[pretalx.plugin]
-{{cookiecutter.module_name}}={{cookiecutter.module_name}}:PretalxPluginMeta
-""",
-)
+setup()

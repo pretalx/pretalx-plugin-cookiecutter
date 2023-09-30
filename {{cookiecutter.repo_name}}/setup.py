03412e97672f714b92ca5f626b72f275246ebc26
Tobias Kunze <r@rixx.de>
2023-09-30 13:01:20 +0200

    Revert "Move to pyproject.toml"
    This reverts commit efa2875b54e1fb9a9bb43c8cd252960b0b922dc5.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,3 +1,42 @@
-from setuptools import setup
+import os
+from distutils.command.build import build
 
-setup()
+from django.core import management
+from setuptools import find_packages, setup
+
+try:
+    with open(
+        os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
+    ) as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command("compilemessages", verbosity=1)
+        build.run(self)
+
+
+cmdclass = {"build": CustomBuild}
+
+
+setup(
+    name="{{cookiecutter.repo_name}}",
+    version="0.0.0",
+    description="{{cookiecutter.short_description}}",
+    long_description=long_description,
+    url="{{cookiecutter.repo_url}}",
+    author="{{cookiecutter.author_name}}",
+    author_email="{{cookiecutter.author_email}}",
+    license="Apache Software License",
+    install_requires=[],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretalx.plugin]
+{{cookiecutter.module_name}}={{cookiecutter.module_name}}:PretalxPluginMeta
+""",
+)

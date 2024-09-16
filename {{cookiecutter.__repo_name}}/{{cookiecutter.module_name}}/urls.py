bccd601ef2b7766a70867fbf979138a2fa625830
Tobias Kunze <r@rixx.de>
2024-09-16 11:47:37 +0200

    Fix broken default urls.py

--- {{cookiecutter.__repo_name}}/{{cookiecutter.module_name}}/urls.py
+++ {{cookiecutter.__repo_name}}/{{cookiecutter.module_name}}/urls.py
@@ -1,13 +1,14 @@
 {% if cookiecutter.category != "LANGUAGE" %}
 from django.urls import re_path
+
 from pretalx.event.models.event import SLUG_REGEX
 
-from .views import {{ cookiecutter.__model_name }}Settings
+from .views import {{ cookiecutter.__model_name }}SettingsView
 
 urlpatterns = [
     re_path(
         rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/{{ cookiecutter.module_name }}/$",
-        {{ cookiecutter.__model_name }}Settings.as_view(),
+        {{ cookiecutter.__model_name }}SettingsView.as_view(),
         name="settings",
     ),
 ]

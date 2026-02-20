05f66bc227f7e9d9e48dbd4e3f8a56dab7755d63
Tobias Kunze <r@rixx.de>
2026-02-20 09:49:32 +0100

    Do not provide github workflow if not on github

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,8 @@
+import shutil
 import subprocess
+from pathlib import Path
+
+if "github.com" not in "{{ cookiecutter.repo_url }}":
+    shutil.rmtree(Path(".github"))
 
 subprocess.run(["just", "fmt"], check=False)

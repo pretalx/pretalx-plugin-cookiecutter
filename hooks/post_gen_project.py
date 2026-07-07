b8e8748271dd74af06e4099d8ca244af8ab2b661
Tobias Kunze <r@rixx.de>
2026-07-07 14:00:38 +0200

    Update justfile to standard plugin justfile

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,5 @@ from pathlib import Path
 if "github.com" not in "{{ cookiecutter.repo_url }}":
     shutil.rmtree(Path(".github"))
 
+subprocess.run(["uv", "lock"], check=False)
 subprocess.run(["just", "fmt"], check=False)
